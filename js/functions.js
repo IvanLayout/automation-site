abc2e21d9203cbfc7945a8f6bf35191e8c07be14
IvanLayuot <ivanpetrachenkov.web@gmail.com>
2026-05-17 11:53:52 +0200

    header

--- js/functions.js
+++ js/functions.js
@@ -82,6 +82,12 @@ $(() => {
 
 			if (is_touch_device()) $('body').css('cursor', 'default')
 		}
+
+		if ( !e.target.closest('.header-catalog') && !e.target.closest('.header-catalog__open') ) {
+			$('.header-catalog__open').removeClass('_active')
+			$('.header-catalog__block').removeClass('_show')
+			$('.overlay-catalog').removeClass('_show')
+		}
 	})
 
 	$('body').on('click', '[data-mini-close]', function(e) {
@@ -220,6 +226,140 @@ $(() => {
 			Thumbs: false,
 		},
 	});
+
+
+
+	$('body').on('click', '.header-catalog__open', function (e) {
+		e.preventDefault()
+
+		if ($(this).hasClass('_active')) {
+			$(this).removeClass('_active')
+			$('.header-catalog__block').removeClass('_show')
+			$('.overlay-catalog').removeClass('_show')
+		} else {
+			$(this).addClass('_active')
+			$('.header-catalog__block').addClass('_show')
+			$('.overlay-catalog').addClass('_show')
+		}
+	})
+
+	$('body').on('click', '.header-submenu__sub-item a._sub', function (e) {
+		e.preventDefault()
+
+		if ($(this).hasClass('_active')) {
+			$(this).removeClass('_active')
+		} else {
+			$(this).addClass('_active')
+		}
+	})
+
+	$('body').on('click', '.header-submenu__sub-more', function (e) {
+		e.preventDefault()
+
+		if ($(this).hasClass('_active')) {
+			$(this).removeClass('_active')
+
+			$(this).closest('.header-submenu__sub').find('._hide').removeClass('_show')
+		} else {
+			$(this).addClass('_active')
+
+			$(this).closest('.header-submenu__sub').find('._hide').addClass('_show')
+		}
+	})
+	
+
+	// Наведение на пункты меню
+	if ( $(window).width() > 1023 ) {
+		$('body').on('mouseover', '.header-menu__item', function (e) {
+			if (!$(this).hasClass('_active-pc')) {
+				$(this).closest('.header-menu').find('.header-menu__item').removeClass('_active-pc')
+	
+				$(this).addClass('_active-pc')
+			}
+		})
+	} else if ( $(window).width() < 1024 && $(window).width() > 767 ) {
+		$('body').on('click', '.header-menu__link._sub', function (e) {
+			e.preventDefault()
+	
+			if (!$(this).closest('.header-menu__item').hasClass('_active-pc')) {
+				$(this).closest('.header-menu').find('.header-menu__item').removeClass('_active-pc')
+	
+				$(this).closest('.header-menu__item').addClass('_active-pc')
+			}
+		})
+	}
+
+	if ( $(window).width() < 768 ) {
+		$('body').on('click', '.header-menu__link._sub', function (e) {
+			e.preventDefault()
+
+			let titleCatalog = $(this).find('.header-menu__link-name').text()
+			$('.catalog-head__title').text(titleCatalog).data('title-second' , titleCatalog)
+
+			$(this).next('.header-submenu').addClass('_show')
+
+			$('.header__info, .catalog-head__back').addClass('_second')
+		})
+
+		$('body').on('click', '.header-submenu__link._sub', function (e) {
+			e.preventDefault()
+
+			let titleCatalog2 = $(this).find('.header-submenu__link-name').text()
+
+			$('.catalog-head__title').text(titleCatalog2)
+
+			$(this).next('.header-submenu__sub').addClass('_show')
+
+			$('.header__info, .catalog-head__back').addClass('_third')
+		})
+	}
+
+	$('body').on('click', '.open-catalog-mob', function (e) {
+		e.preventDefault()
+
+		$('.open-catalog-mob').addClass('_active')
+		$('.header').addClass('_show')
+		$('body').addClass('_menu-open')
+	})
+
+	$('body').on('click', '.catalog-head__close', function (e) {
+		e.preventDefault()
+
+		$('.open-catalog-mob').removeClass('_active')
+		$('.header').removeClass('_show')
+		$('body').removeClass('_menu-open')
+
+		$('.header-submenu').removeClass('_show')
+		$('.header__info, .catalog-head__back').removeClass('_second')
+
+		$('.header-submenu__sub').removeClass('_show')
+		$('.header__info, .catalog-head__back').removeClass('_third')
+
+		let titleCatalog = $('.catalog-head__title').data('title')
+		$('.catalog-head__title').text(titleCatalog)
+	})
+
+	$('body').on('click', '.catalog-head__back', function (e) {
+		e.preventDefault()
+
+		if($(this).hasClass('_third')) {
+			let titleCatalog2 = $('.catalog-head__title').data('title-second')
+			$('.catalog-head__title').text(titleCatalog2)
+
+			$('.header-submenu__sub').removeClass('_show')
+			$('.header__info, .catalog-head__back').removeClass('_third')
+		} else if ($(this).hasClass('_second')) {
+			let titleCatalog = $('.catalog-head__title').data('title')
+			$('.catalog-head__title').text(titleCatalog)
+
+			$('.header-submenu').removeClass('_show')
+			$('.header__info, .catalog-head__back').removeClass('_second')
+		} else {
+			$('.open-catalog-mob').removeClass('_active')
+			$('.header').removeClass('_show')
+			$('body').removeClass('_menu-open')
+		}
+	})
 
 	// commit
 
